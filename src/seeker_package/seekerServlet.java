bf44f2313005227ab21c92b18c5b348ee3c98a0f
shashi ranjan <33440715+trent344@users.noreply.github.com>
2018-09-01 18:46:41 +0530

    seeker-servlet for login and signup of a seeker

---
+++ src/seeker_package/seekerServlet.java
@@ -0,0 +1,73 @@
+package seeker;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+@WebServlet("/s_login_create")
+public class seekerServlet extends HttpServlet{
+	
+	PrintWriter out;
+	workBase base;
+	
+	public void doPost(HttpServletRequest request,HttpServletResponse response) throws IOException {
+		response.setHeader("Cache-Control", "private, no-store, no-cache, must-revalidate");
+		response.setHeader("Pragma", "no-cache");
+		response.setHeader("Expires", "0");
+		
+		out=response.getWriter();
+		
+		String mail=request.getParameter("mail");
+		String key=null;
+		key=request.getParameter("login");
+		int i=0;
+		if(key==null) {
+			key=request.getParameter("signup");
+			i=1;
+		}
+		
+		key=request.getParameter("key");
+		
+		base=new workBase();
+		
+		if(i==0) {//login
+			
+			key=base.isValid("mail", mail, key);
+			if(key.equals("0")) {
+				out.println("wrong mail or password");return;
+			}
+			else if(key.equals("-1")) {
+				out.println("internal error, try again");return;
+			}
+			
+		}else if(i==1) {//sign-up
+			
+			if(!base.insertLog(mail, key)) {
+				out.println(" perhaps your mail exists, try login");return;
+			}
+			
+			System.out.println("seeker servlet()  && i=1;");
+			
+			key="0";
+			
+		}else {
+			response.sendRedirect("index.html");return;
+		}
+		
+		base.disconnect();
+		
+		HttpSession session=request.getSession();
+		session.setAttribute("mail", mail);
+		session.setAttribute("ids", key);
+		
+		response.sendRedirect("seeker_home");
+		
+	}
+
+
+}
